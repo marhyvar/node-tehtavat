b634eb7ad9b80342aa4023f6c7c62633f027aa9a
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-04-18 10:52:08 +0300

    Add data handling after GET request

--- tehtavat-3/app.js
+++ tehtavat-3/app.js
@@ -10,21 +10,31 @@ const url = 'https://www.ilmatieteenlaitos.fi/observation-data?station=101004'
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded ({ extended : true }))
-app.get('/weather', query_validator, (req, res) => {
+app.get('/weather', query_validator, (req, res, next) => {
     //res.status(200).send(req.query.observation);
     fetch(url)
         .then(res => res.json())
-        .then(json => res.status(200).send(json))
+        .then(json => useData(json))
+        .then(json => res.send(json))
+
 });
 app.use(error_handler);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
+const useData = json => {
+    let obj = {};
+    obj['temperature'] = json['t2m']
+    obj['humidity'] = json['Humidity']
+    obj['wind'] = json['WindSpeedMS']
+    return obj;
+}
+
 const makeRequest = async url => {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        res.status(200).send(data);
+        return data;
     } catch (err) {
         console.log(err);
     }
