5e0af8c21fc321130900ad840018f696236e2834
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-04-18 21:25:46 +0300

    Add module for JSON handling

--- tehtavat-3/app.js
+++ tehtavat-3/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const fetch = require('node-fetch');
 const query_validator = require('./query_validator');
 const error_handler = require('./error_handler');
+const weather = require('./weather');
 const app = express();
 const port = 3000;
 const url = 'https://www.ilmatieteenlaitos.fi/observation-data?station=101004'
@@ -13,7 +14,7 @@ app.use(express.urlencoded ({ extended : true }))
 app.get('/weather', query_validator, (req, res, next) => {
     fetch(url)
         .then(res => res.json())
-        .then(json => useData(json, req.query))
+        .then(json => weather.useData(json, req.query))
         .then(json => res.send(json))
 
 });
@@ -21,28 +22,4 @@ app.use(error_handler);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
-const useData = (json, weather) => {
-    let obj = {};
-    if (Object.keys(weather).length === 0) {
-        obj['temperature'] = getLatest(json, 't2m')
-        obj['humidity'] = getLatest(json, 'Humidity');
-        obj['wind'] = getLatest(json, 'WindSpeedMS');
-        return obj;
-    } else {
-        const items = weather.observation;
-        for (const key in items) {
-            const value = items[key]
-            obj[value] = getLatest(json, weatherItems[value])
-        }
-        return obj;
-    }
-}
 
-const getLatest = (data, item) => 
-    data[item][(data[item].length -1)][1] 
-
-const weatherItems = {
-    temperature: 't2m',
-    humidity: 'Humidity',
-    wind: 'WindSpeedMS'
-}
