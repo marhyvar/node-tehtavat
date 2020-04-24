2c24dcf65e1bbe3f5df8ed52eb42bde949ae8195
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-04-24 19:38:15 +0300

    Add authentication for mockup-user

--- tehtavat-4/routes/auth.js
+++ tehtavat-4/routes/auth.js
@@ -1,8 +1,16 @@
+const tokenMaker = require('../tokens');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('hello');
+router.post('/', (req, res) => {
+    const user = req.body.username;
+    const pwd = req.body.password;
+    if (user === "mark" && pwd === 'giraffe') {
+        let token = tokenMaker.create(user)
+        res.status(200).send({ token })
+    } else {
+        res.status(400).send({ 'error': 'Invalid credentials' });
+    }
 });
 
 module.exports = router;
