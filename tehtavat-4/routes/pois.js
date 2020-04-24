1a118006d118d6b9befe68746b708dde47bc37ac
M. Hyvarinen <54714032+marhyvar@users.noreply.github.com>
2020-04-24 15:21:39 +0300

    Add post id route

--- tehtavat-4/routes/pois.js
+++ tehtavat-4/routes/pois.js
@@ -15,11 +15,33 @@ router.get('/:id', (req, res) => {
         if (typeof poi != 'undefined') {
             res.status(200).send(poi);
         } else {
-            res.status(404).send('id:tä ei ole');
+            res.status(404).send({404: 'id:tä ei ole'});
         }
         
     } catch (e) {
         console.log('error');       
+    }
+})
+
+router.post('/', (req, res) => {
+    const name = req.body.name;
+    const description = req.body.description;
+    const city = req.body.city;
+    const lat = req.body.coordinates.lat;
+    const lng = req.body.coordinates.lng;
+    if (Object.keys(req.body).length != 4) {
+        res.status(400).send({400: 'POI-tiedot virheelliset'});    
+    } else {
+        if (name !== '' && city !=='' && description !== '') {
+            if (isNaN(lat) || isNaN(lng)) {
+                res.status(400).send({400: 'POI-tiedot virheelliset'});
+            } else {
+                db.createPoi(req.body);
+                res.status(201).send(req.body);
+            }
+        } else {
+            res.status(400).send({400: 'POI-tiedot virheelliset'});
+        }
     }
 })
 
